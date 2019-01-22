61fdd7ced2db55584718c8aab1aa8f24d1d49e2a
Jesse Ross <sora4222@gmail.com>
2019-01-22 21:58:43 +1100

    Converts U-net to a functional api methodology as the network requires that for concatenation.

--- generate_u_net.py
+++ generate_u_net.py
@@ -1,12 +1,11 @@
 from keras import Sequential
 from keras import layers as layers
+import keras
 from typing import List
-
 BATCH_SIZE: int = 1
 IMAGE_SIZE: List[int] = [768, 768, 3]
 
-
-def add_downsample_layer(model: Sequential, filters: int):
+def add_downsample_layer(layer: keras.layers.Layer, filters: int):
     """
 
     :param model:
@@ -15,39 +14,87 @@ def add_downsample_layer(model: Sequential, filters: int):
     """
     # padding "valid" means no padding
     cnn_layer_1 = layers.Conv2D(filters, kernel_size=(3, 3), strides=(1, 1),
-                                padding="valid", activation="relu")
+                                padding="same", activation="relu", data_format="channels_last")(layer)
+    cnn_layer_1_norm = layers.BatchNormalization()(cnn_layer_1)
     cnn_layer_2 = layers.Conv2D(filters, kernel_size=(3, 3), strides=(1, 1),
-                                padding="valid", activation="relu")
-    sample_down_layer = layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding="valid")
-    model.add(cnn_layer_1)
-    model.add(cnn_layer_2)
-    model.add(sample_down_layer)
-    return model, cnn_layer_2
+                                padding="same", activation="relu", data_format="channels_last")(cnn_layer_1_norm)
+    cnn_layer_2_norm = layers.BatchNormalization()(cnn_layer_2)
+    sample_down_layer = layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding="valid",
+                                         data_format="channels_last")(cnn_layer_2_norm)
+    return sample_down_layer, cnn_layer_2
 
 
-def add_expansive_layer(model: Sequential, filters_to_begin_with: int,
-                        downsample_input_layer: layers.Conv2D) -> Sequential:
+def add_expansive_layer(input_layer, filters_to_begin_with: int,
+                        downsample_input_layer) -> Sequential:
     # Upsample
-    upsample_layer = layers.Conv2DTranspose(filters_to_begin_with, kernel_size=(2, 2))
-    concatenation_layer = layers.concatenate([upsample_layer, downsample_input_layer])
-    cnn_layer_1 = layers.Conv2D(filters=filters_to_begin_with / 2, kernel_size=(3, 3), activation="relu")
-    cnn_layer_2 = layers.Conv2D(filters=filters_to_begin_with / 2, kernel_size=(3, 3), activation="relu")
-    model.add(upsample_layer)
-    model.add(concatenation_layer)
-    model.add(cnn_layer_1)
-    model.add(cnn_layer_2)
-    return model
+    upsample_layer = layers.Conv2DTranspose(filters_to_begin_with, kernel_size=(2, 2),
+                                            data_format="channels_last")(input_layer)
+
+    # Crop the downsample_input_layer down to concatenate with the previous
+    # crop_height, crop_width = get_crop_shape(downsample_input_layer, upsample_layer)
+    concatenation_layer = layers.concatenate([upsample_layer, downsample_input_layer], axis=-1)
+    cnn_layer_1 = layers.Conv2D(filters=filters_to_begin_with // 2,
+                                kernel_size=(3, 3), activation="relu",
+                                data_format="channels_last",
+                                padding="same")(concatenation_layer)
+    cnn_layer_1_norm = layers.BatchNormalization()(cnn_layer_1)
+    cnn_layer_2 = layers.Conv2D(filters=filters_to_begin_with // 2,
+                                kernel_size=(3, 3),
+                                activation="relu",
+                                data_format="channels_last",
+                                padding="same")(cnn_layer_1_norm)
+    cnn_layer_2_norm = layers.BatchNormalization()(cnn_layer_2)
+    return cnn_layer_2_norm
+
+
+# def get_crop_shape(target: layers.Layer, refer: layers.Layer):
+#     # width, the 3rd dimension
+#     cw = (target.get_shape()[2] - refer.output_shape[2]).value
+#     assert (cw >= 0)
+#     if cw % 2 != 0:
+#         cw1, cw2 = int(cw / 2), int(cw / 2) + 1
+#     else:
+#         cw1, cw2 = int(cw / 2), int(cw / 2)
+#     # height, the 2nd dimension
+#     ch = (target.get_shape()[1] - refer.get_shape()[1]).value
+#     assert (ch >= 0)
+#     if ch % 2 != 0:
+#         ch1, ch2 = int(ch / 2), int(ch / 2) + 1
+#     else:
+#         ch1, ch2 = int(ch / 2), int(ch / 2)
+#
+#     return (ch1, ch2), (cw1, cw2)
 
 
 if __name__ == '__main__':
-    model: Sequential = Sequential()
     INITIAL_SIZE = 64
-    model.add(layers.InputLayer(input_shape=IMAGE_SIZE))
-    model, layer_1 = add_downsample_layer(model, INITIAL_SIZE)
-    model, layer_2 = add_downsample_layer(model, INITIAL_SIZE*2)
+    input_tensor = layers.Input(shape=IMAGE_SIZE, name="Input_tensor")
 
-    model.add(layers.Conv2D(INITIAL_SIZE*4))
-    model.add(layers.Conv2D(INITIAL_SIZE*4))
+    input_normal = layers.BatchNormalization()(input_tensor)
+    layer_1_downsampled, layer_1 = add_downsample_layer(input_normal, INITIAL_SIZE)
+    print("layer 1: ", keras.backend.int_shape(layer_1))
+    print("layer 1 downsampled:", keras.backend.int_shape(layer_1_downsampled))
 
-    model = add_expansive_layer(model, INITIAL_SIZE*4, layer_2)
-    model = add_expansive_layer(model, INITIAL_SIZE*2, layer_1)
+    layer_2_downsampled, layer_2 = add_downsample_layer(layer_1_downsampled, INITIAL_SIZE*2)
+    layer_2_downsampled_norm = layers.BatchNormalization()(layer_2_downsampled)
+    print("layer 2: ", keras.backend.int_shape(layer_2))
+    print("layer 2 downsampled:", keras.backend.int_shape(layer_2_downsampled))
+
+    layer_3 = layers.Conv2D(INITIAL_SIZE*4, (3, 3), padding="same")(layer_2_downsampled_norm)
+    layer_3_norm = layers.BatchNormalization()(layer_3)
+    print("layer 3: ", keras.backend.int_shape(layer_2))
+
+    layer_3 = layers.Conv2D(INITIAL_SIZE*4, (3, 3), padding="same")(layer_3_norm)
+    layer_3_norm = layers.BatchNormalization()(layer_3)
+    print("layer 3 norm", keras.backend.int_shape(layer_3_norm))
+
+    layer_4_upsampled = add_expansive_layer(layer_3_norm, INITIAL_SIZE*4, layer_2)
+    layer_5_upsampled = add_expansive_layer(layer_4_upsampled, INITIAL_SIZE*2, layer_1)
+
+    output_layer = layers.Conv2D(1, (1, 1), activation="sigmoid", padding="same")(layer_5_upsampled)
+
+    model: Sequential = Sequential(layers=output_layer)
+    model.compile()
+    model.summary()
+
+
