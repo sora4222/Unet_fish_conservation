cc398d8d3f0e5e8f083ff2c77f8349e7d7e825f1
Jesse Ross <sora4222@gmail.com>
2019-02-17 22:06:33 +1100

    The neural network now crops and concatenates the jump connections

--- generate_u_net.py
+++ generate_u_net.py
@@ -1,110 +1,144 @@
-from typing import List
+from typing import List, Tuple
 
-import keras
-from keras import Model
-from keras import layers as layers
-from keras.losses import binary_crossentropy
-from keras.optimizers import Adam
+from tensorflow.python import keras
+from tensorflow.python.keras import layers
 from input_output_generators import generate_image_data, SEGMENTATION_DIRECTORY_LOCATION, save_image
 import logging
 import numpy as np
+from model.utils.crop_functions import get_crop_dimensions
 
 BATCH_SIZE: int = 2
 IMAGE_SIZE: List[int] = [768, 768, 3]
+PADDING: str = "VALID"
 
 
 def add_downsample_layer(layer: keras.layers.Layer, filters: int):
     """
-
+    Adds a downsample set of Convolutional2D layers.
     :param model:
     :param filters:
-    :return: model, the second cnn network
+    :return: the downsampled layer, the conv2d layer just before
     """
     # padding "valid" means no padding
-    cnn_layer_1 = layers.Conv2D(filters, kernel_size=(3, 3), strides=(1, 1),
-                                padding="same", activation="relu", data_format="channels_last")(layer)
+    cnn_layer_1 = layers.Conv2D(filters,
+                                kernel_size=(3, 3),
+                                strides=(1, 1),
+                                padding=PADDING,
+                                activation="relu",
+                                data_format="channels_last")(layer)
+    logging.debug(f"add_downsample_layer: cnn_layer_1 shape: {cnn_layer_1.get_shape()}")
     cnn_layer_1_norm = layers.BatchNormalization()(cnn_layer_1)
-    cnn_layer_2 = layers.Conv2D(filters, kernel_size=(3, 3), strides=(1, 1),
-                                padding="same", activation="relu", data_format="channels_last")(cnn_layer_1_norm)
+    cnn_layer_2 = layers.Conv2D(filters,
+                                kernel_size=(3, 3),
+                                strides=(1, 1),
+                                padding=PADDING,
+                                activation="relu",
+                                data_format="channels_last")(cnn_layer_1_norm)
+    logging.debug(f"add_downsample_layer: cnn_layer_2 shape (skip connection): {cnn_layer_2.get_shape()[2]}")
     cnn_layer_2_norm = layers.BatchNormalization()(cnn_layer_2)
-    sample_down_layer = layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding="same",
+    sample_down_layer = layers.MaxPool2D(pool_size=(2, 2),
+                                         strides=(2, 2),
+                                         padding=PADDING,
                                          data_format="channels_last")(cnn_layer_2_norm)
     return sample_down_layer, cnn_layer_2
 
 
 def add_expansive_layer(input_layer, filters_to_end_with: int,
-                        downsample_input_layer) -> layers.Layer:
-    print("filters: ", filters_to_end_with)
+                        jump_connections) -> layers.Layer:
+    logging.debug(f"filters: {filters_to_end_with}")
     # Upsample
-    upsample_layer = layers.Conv2DTranspose(filters_to_end_with, kernel_size=(2, 2),
+    logging.debug(f"add_expansive_layer : input_layer layer shape: {input_layer.get_shape()}")
+    upsample_layer = layers.Conv2DTranspose(filters_to_end_with,
+                                            kernel_size=(2, 2),
+                                            padding=PADDING,
                                             data_format="channels_last",
                                             strides=(2, 2))(input_layer)
+    logging.debug(f"add_expansive_layer : Upsample layer shape: {upsample_layer.get_shape()}")
 
-    print("Upsample layer: ", keras.backend.int_shape(upsample_layer))
-    print("Input layer to concatenate: ", keras.backend.int_shape(downsample_input_layer))
-    concatenation_layer = layers.concatenate([upsample_layer, downsample_input_layer])
+    crop_dimensions: Tuple[Tuple[int], Tuple[int]] = get_crop_dimensions(to_crop=jump_connections,
+                                                                         layer_to_crop_to=upsample_layer)
+    crop_layer = layers.Cropping2D(cropping=crop_dimensions)(jump_connections)
+    concatenation_layer = layers.concatenate([upsample_layer, crop_layer])
+    logging.debug(f"Come out of concat with: {keras.backend.int_shape(concatenation_layer)}")
     cnn_layer_1 = layers.Conv2D(filters=filters_to_end_with // 2,
-                                kernel_size=(3, 3), activation="relu",
+                                kernel_size=(3, 3),
+                                activation="relu",
                                 data_format="channels_last",
-                                padding="same")(concatenation_layer)
+                                padding=PADDING)(concatenation_layer)
     cnn_layer_1_norm = layers.BatchNormalization()(cnn_layer_1)
     cnn_layer_2 = layers.Conv2D(filters=filters_to_end_with // 2,
                                 kernel_size=(3, 3),
                                 activation="relu",
                                 data_format="channels_last",
-                                padding="same")(cnn_layer_1_norm)
+                                padding=PADDING)(cnn_layer_1_norm)
     cnn_layer_2_norm: layers.Layer = layers.BatchNormalization()(cnn_layer_2)
     return cnn_layer_2_norm
 
 
 # try: https://www.kaggle.com/keegil/keras-u-net-starter-lb-0-277
+def log_shape(layer_name: str, layer) -> None:
+    logging.info(f"{layer_name} has shape: {layer.get_shape()}")
+
+
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format='%(name)s - %(levelname)s - %(message)s')
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(name)s - %(levelname)s - %(message)s',
+                        filename="logs.txt",
+                        filemode="w+")
     INITIAL_SIZE = 64
     input_tensor: layers.Layer = layers.Input(shape=IMAGE_SIZE, name="Input_tensor")
 
-    print("")
-    print("Going down the U-net:...")
+    logging.debug("Going down the U-net:...")
 
     input_normal = layers.BatchNormalization()(input_tensor)
     layer_1_downsampled, layer_1 = add_downsample_layer(input_normal, INITIAL_SIZE)
-    print("layer 1 downsampled:", keras.backend.int_shape(layer_1_downsampled))
-    print("layer 1: ", keras.backend.int_shape(layer_1))
+    logging.debug(f"layer 1 downsampled: {keras.backend.int_shape(layer_1_downsampled)}")
+    logging.debug(f"layer 1: {keras.backend.int_shape(layer_1)}")
+    log_shape("layer_1_downsampled", layer_1_downsampled)
+    log_shape("layer_1", layer_1)
 
     layer_2_downsampled, layer_2 = add_downsample_layer(layer_1_downsampled, INITIAL_SIZE * 2)
     layer_2_downsampled_norm = layers.BatchNormalization()(layer_2_downsampled)
-    print("layer 2 downsampled:", keras.backend.int_shape(layer_2_downsampled))
-    print("layer 2: ", keras.backend.int_shape(layer_2))
+    logging.debug(f"layer 2 downsampled: {keras.backend.int_shape(layer_2_downsampled)}")
+    logging.debug(f"layer 2: {keras.backend.int_shape(layer_2)}")
+    log_shape("layer_2_downsampled", layer_2_downsampled)
+    log_shape("layer_2", layer_2)
 
     layer_3_downsampled, layer_3 = add_downsample_layer(layer_2_downsampled_norm, INITIAL_SIZE * 4)
     layer_3_downsampled_norm = layers.BatchNormalization()(layer_3_downsampled)
-    print("layer 3 downsampled:", keras.backend.int_shape(layer_3_downsampled))
-    print("layer 3: ", keras.backend.int_shape(layer_3))
+    logging.debug(f"layer 3 downsampled: {keras.backend.int_shape(layer_3_downsampled)}")
+    logging.debug(f"layer 3: {keras.backend.int_shape(layer_3)}")
+    log_shape("layer_3_downsampled", layer_3_downsampled)
+    log_shape("layer_3", layer_3)
 
     layer_4_downsampled, layer_4 = add_downsample_layer(layer_3_downsampled_norm, INITIAL_SIZE * 8)
     layer_4_downsampled_norm = layers.BatchNormalization()(layer_4_downsampled)
-    print("layer 4 downsampled:", keras.backend.int_shape(layer_4_downsampled))
-    print("layer 4: ", keras.backend.int_shape(layer_4))
+    logging.debug(f"layer 4 downsampled: {keras.backend.int_shape(layer_4_downsampled)}")
+    logging.debug(f"layer 4: {keras.backend.int_shape(layer_4)}")
+    log_shape("layer_4_downsampled", layer_4_downsampled)
+    log_shape("layer_4", layer_4)
 
-    layer_5 = layers.Conv2D(INITIAL_SIZE * 16, (3, 3), padding="same")(layer_4_downsampled_norm)
+    layer_5 = layers.Conv2D(INITIAL_SIZE * 16, (3, 3), padding=PADDING)(layer_4_downsampled_norm)
     layer_5_norm = layers.BatchNormalization()(layer_5)
-    print("layer 5 norm", keras.backend.int_shape(layer_5_norm))
+    logging.debug(f"layer 5 norm {keras.backend.int_shape(layer_5_norm)}")
 
-    print()
-    print("Going back up the U-net")
+    logging.debug("Going back up the U-net")
+    logging.debug("layer 6")
     layer_6_upsampled = add_expansive_layer(layer_5_norm, INITIAL_SIZE * 8, layer_4)
+    logging.debug("layer 7")
     layer_7_upsampled = add_expansive_layer(layer_6_upsampled, INITIAL_SIZE * 4, layer_3)
+    logging.debug("layer 8")
     layer_8_upsampled = add_expansive_layer(layer_7_upsampled, INITIAL_SIZE * 2, layer_2)
     layer_9_upsampled = add_expansive_layer(layer_8_upsampled, INITIAL_SIZE, layer_1)
 
     # activation softmax has higher loss but lower val_loss
-    output_layer = layers.Conv2D(1, (1, 1), activation="sigmoid", padding="same")(layer_9_upsampled)
+    output_layer = layers.Conv2D(1, (1, 1), activation="sigmoid", padding=PADDING)(layer_9_upsampled)
 
-    model: Model = Model(inputs=[input_tensor], outputs=[output_layer])
+    model: keras.Model = keras.Model(inputs=[input_tensor], outputs=[output_layer])
     model.summary()
 
-    optimizer = Adam(lr=0.001)
-    model.compile(optimizer=optimizer, loss=binary_crossentropy)
+    optimizer = keras.optimizers.Adam(lr=0.001)
+    model.compile(optimizer=optimizer, loss=keras.losses.binary_crossentropy)
 
     # TODO: Add validation data and histogram=x where x > 0
     tensorboard_callback = keras.callbacks.TensorBoard(
@@ -112,8 +146,7 @@ if __name__ == '__main__':
         batch_size=1,
         histogram_freq=0,
         write_graph=True,
-        write_images=False,
-        update_freq='batch')
+        write_images=False)
 
     reduce_learning_rate_callback = keras.callbacks.ReduceLROnPlateau(patience=3,
                                                                       mode="min")
---
+++ model/utils/crop_functions.py
@@ -0,0 +1,41 @@
+import keras as K
+import tensorflow as tf
+from typing import Tuple
+import logging
+
+def correct_dimension(dimension_size: int) -> Tuple[int, int]:
+    """
+    Corrects the dimensions for an image that has an odd height or width
+    :param dimension_size:
+    :return:
+    """
+    assert dimension_size >= 0, "The dimension size is less than 0"
+    if dimension_size % 2 != 0:
+        logging.debug("Odd number of dimensions")
+        return int(dimension_size / 2), int(dimension_size / 2) + 1
+    else:
+        logging.debug("Even number of dimensions")
+        return int(dimension_size / 2), int(dimension_size / 2)
+
+
+def get_crop_dimensions(to_crop: tf.Tensor, layer_to_crop_to: tf.Tensor) -> Tuple[Tuple[int, int], Tuple[int, int]]:
+    """
+    Obtained from
+    https://github.com/zizhaozhang/unet-tensorflow-keras/blob/master/model.py
+    slices the to_crop layer to be the same size as the layer_to_concatenate_to layer.
+    :type to_crop: tf.Tensor
+    :param to_crop: The Tensor to crop.
+    :param layer_to_crop_to: The Tensor to match the dimensions of.
+    :return: The number of rows and columns to remove in the format keras.layers.Cropping2D uses
+    """
+    logging.debug("get_crop_dimensions")
+    with tf.name_scope("crop_and_concat"):
+        logging.debug(f"{__name__}: to_crop: {to_crop.get_shape()[2]}")
+        logging.debug(f"{__name__}: layer_to_crop: {layer_to_crop_to.get_shape()[2]}")
+        change_width: int = (to_crop.get_shape()[2] - layer_to_crop_to.get_shape()[2]).value
+        change_height: int = (to_crop.get_shape()[1] - layer_to_crop_to.get_shape()[1]).value
+        logging.debug(f"Change width {change_width}")
+        logging.debug(f"Change height {change_height}")
+        corrected_width: Tuple[int, int] = correct_dimension(change_width)
+        corrected_height: Tuple[int, int] = correct_dimension(change_height)
+        return corrected_height, corrected_width
